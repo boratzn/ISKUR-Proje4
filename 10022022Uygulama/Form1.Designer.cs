e15ac46086f6f951fbf05a69977244815dd5cac7
Bora TÜZÜN <boratzn94@gmail.com>
2022-02-15 13:38:24 +0300

    Add files via upload

---
+++ 10022022Uygulama/Form1.Designer.cs
@@ -0,0 +1,303 @@
+﻿namespace _10022022Uygulama
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.tbMusteriNo = new System.Windows.Forms.TextBox();
+            this.tbUnvan = new System.Windows.Forms.TextBox();
+            this.tbTelefon = new System.Windows.Forms.TextBox();
+            this.tbAdres = new System.Windows.Forms.TextBox();
+            this.tbBakiye = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.tbMail = new System.Windows.Forms.TextBox();
+            this.btnOncekiKayit = new System.Windows.Forms.Button();
+            this.btnSonrakiKayit = new System.Windows.Forms.Button();
+            this.btnAranan = new System.Windows.Forms.Button();
+            this.tbAranan = new System.Windows.Forms.TextBox();
+            this.btnKaydet = new System.Windows.Forms.Button();
+            this.btnSil = new System.Windows.Forms.Button();
+            this.btnDüzenle = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(17, 16);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Müşteri No";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(16, 48);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 16);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Ünvan";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(16, 80);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 16);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Telefon";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(16, 112);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(43, 16);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Adres";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(16, 144);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(49, 16);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "Bakiye";
+            // 
+            // tbMusteriNo
+            // 
+            this.tbMusteriNo.Location = new System.Drawing.Point(115, 12);
+            this.tbMusteriNo.Margin = new System.Windows.Forms.Padding(4);
+            this.tbMusteriNo.Name = "tbMusteriNo";
+            this.tbMusteriNo.Size = new System.Drawing.Size(132, 22);
+            this.tbMusteriNo.TabIndex = 1;
+            // 
+            // tbUnvan
+            // 
+            this.tbUnvan.Location = new System.Drawing.Point(115, 44);
+            this.tbUnvan.Margin = new System.Windows.Forms.Padding(4);
+            this.tbUnvan.Name = "tbUnvan";
+            this.tbUnvan.Size = new System.Drawing.Size(132, 22);
+            this.tbUnvan.TabIndex = 1;
+            // 
+            // tbTelefon
+            // 
+            this.tbTelefon.Location = new System.Drawing.Point(115, 76);
+            this.tbTelefon.Margin = new System.Windows.Forms.Padding(4);
+            this.tbTelefon.Name = "tbTelefon";
+            this.tbTelefon.Size = new System.Drawing.Size(132, 22);
+            this.tbTelefon.TabIndex = 1;
+            // 
+            // tbAdres
+            // 
+            this.tbAdres.Location = new System.Drawing.Point(115, 108);
+            this.tbAdres.Margin = new System.Windows.Forms.Padding(4);
+            this.tbAdres.Name = "tbAdres";
+            this.tbAdres.Size = new System.Drawing.Size(132, 22);
+            this.tbAdres.TabIndex = 1;
+            // 
+            // tbBakiye
+            // 
+            this.tbBakiye.Location = new System.Drawing.Point(115, 140);
+            this.tbBakiye.Margin = new System.Windows.Forms.Padding(4);
+            this.tbBakiye.Name = "tbBakiye";
+            this.tbBakiye.Size = new System.Drawing.Size(132, 22);
+            this.tbBakiye.TabIndex = 1;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(16, 176);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(32, 16);
+            this.label6.TabIndex = 0;
+            this.label6.Text = "Mail";
+            // 
+            // tbMail
+            // 
+            this.tbMail.Location = new System.Drawing.Point(115, 172);
+            this.tbMail.Margin = new System.Windows.Forms.Padding(4);
+            this.tbMail.Name = "tbMail";
+            this.tbMail.Size = new System.Drawing.Size(132, 22);
+            this.tbMail.TabIndex = 1;
+            // 
+            // btnOncekiKayit
+            // 
+            this.btnOncekiKayit.Location = new System.Drawing.Point(611, 185);
+            this.btnOncekiKayit.Margin = new System.Windows.Forms.Padding(4);
+            this.btnOncekiKayit.Name = "btnOncekiKayit";
+            this.btnOncekiKayit.Size = new System.Drawing.Size(123, 85);
+            this.btnOncekiKayit.TabIndex = 3;
+            this.btnOncekiKayit.Text = "Önceki Kayıt";
+            this.btnOncekiKayit.UseVisualStyleBackColor = true;
+            this.btnOncekiKayit.Click += new System.EventHandler(this.btnOncekiKayit_Click);
+            // 
+            // btnSonrakiKayit
+            // 
+            this.btnSonrakiKayit.Location = new System.Drawing.Point(741, 185);
+            this.btnSonrakiKayit.Margin = new System.Windows.Forms.Padding(4);
+            this.btnSonrakiKayit.Name = "btnSonrakiKayit";
+            this.btnSonrakiKayit.Size = new System.Drawing.Size(123, 85);
+            this.btnSonrakiKayit.TabIndex = 3;
+            this.btnSonrakiKayit.Text = "Sonraki Kayıt";
+            this.btnSonrakiKayit.UseVisualStyleBackColor = true;
+            this.btnSonrakiKayit.Click += new System.EventHandler(this.btnSonrakiKayit_Click);
+            // 
+            // btnAranan
+            // 
+            this.btnAranan.Location = new System.Drawing.Point(17, 208);
+            this.btnAranan.Margin = new System.Windows.Forms.Padding(4);
+            this.btnAranan.Name = "btnAranan";
+            this.btnAranan.Size = new System.Drawing.Size(100, 28);
+            this.btnAranan.TabIndex = 4;
+            this.btnAranan.Text = "Aranan";
+            this.btnAranan.UseVisualStyleBackColor = true;
+            this.btnAranan.Click += new System.EventHandler(this.btnAranan_Click);
+            // 
+            // tbAranan
+            // 
+            this.tbAranan.Location = new System.Drawing.Point(125, 210);
+            this.tbAranan.Margin = new System.Windows.Forms.Padding(4);
+            this.tbAranan.Name = "tbAranan";
+            this.tbAranan.Size = new System.Drawing.Size(207, 22);
+            this.tbAranan.TabIndex = 5;
+            // 
+            // btnKaydet
+            // 
+            this.btnKaydet.Location = new System.Drawing.Point(17, 245);
+            this.btnKaydet.Margin = new System.Windows.Forms.Padding(4);
+            this.btnKaydet.Name = "btnKaydet";
+            this.btnKaydet.Size = new System.Drawing.Size(100, 28);
+            this.btnKaydet.TabIndex = 6;
+            this.btnKaydet.Text = "Kaydet";
+            this.btnKaydet.UseVisualStyleBackColor = true;
+            this.btnKaydet.Click += new System.EventHandler(this.btnKaydet_Click);
+            // 
+            // btnSil
+            // 
+            this.btnSil.Location = new System.Drawing.Point(125, 245);
+            this.btnSil.Margin = new System.Windows.Forms.Padding(4);
+            this.btnSil.Name = "btnSil";
+            this.btnSil.Size = new System.Drawing.Size(100, 28);
+            this.btnSil.TabIndex = 6;
+            this.btnSil.Text = "Sil";
+            this.btnSil.UseVisualStyleBackColor = true;
+            this.btnSil.Click += new System.EventHandler(this.btnSil_Click);
+            // 
+            // btnDüzenle
+            // 
+            this.btnDüzenle.Location = new System.Drawing.Point(233, 245);
+            this.btnDüzenle.Margin = new System.Windows.Forms.Padding(4);
+            this.btnDüzenle.Name = "btnDüzenle";
+            this.btnDüzenle.Size = new System.Drawing.Size(100, 28);
+            this.btnDüzenle.TabIndex = 6;
+            this.btnDüzenle.Text = "Düzenle";
+            this.btnDüzenle.UseVisualStyleBackColor = true;
+            this.btnDüzenle.Click += new System.EventHandler(this.btnDüzenle_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(254, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(610, 164);
+            this.listBox1.TabIndex = 7;
+            this.listBox1.Click += new System.EventHandler(this.listBox1_Click);
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(881, 287);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.btnDüzenle);
+            this.Controls.Add(this.btnSil);
+            this.Controls.Add(this.btnKaydet);
+            this.Controls.Add(this.tbAranan);
+            this.Controls.Add(this.btnAranan);
+            this.Controls.Add(this.btnSonrakiKayit);
+            this.Controls.Add(this.btnOncekiKayit);
+            this.Controls.Add(this.tbMail);
+            this.Controls.Add(this.tbBakiye);
+            this.Controls.Add(this.tbAdres);
+            this.Controls.Add(this.tbTelefon);
+            this.Controls.Add(this.tbUnvan);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.tbMusteriNo);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox tbMusteriNo;
+        private System.Windows.Forms.TextBox tbUnvan;
+        private System.Windows.Forms.TextBox tbTelefon;
+        private System.Windows.Forms.TextBox tbAdres;
+        private System.Windows.Forms.TextBox tbBakiye;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox tbMail;
+        private System.Windows.Forms.Button btnOncekiKayit;
+        private System.Windows.Forms.Button btnSonrakiKayit;
+        private System.Windows.Forms.Button btnAranan;
+        private System.Windows.Forms.TextBox tbAranan;
+        private System.Windows.Forms.Button btnKaydet;
+        private System.Windows.Forms.Button btnSil;
+        private System.Windows.Forms.Button btnDüzenle;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ 10022022Uygulama/Form1.cs
@@ -0,0 +1,222 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _10022022Uygulama
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            btnOncekiKayit.Enabled = false;
+            btnSonrakiKayit.Enabled=false;
+        }
+        int musteriNo;
+        string unvan;
+        string telNo;
+        string adres;
+        double bakiye;
+        string mail;
+        //int[] musteriNo2;
+        //string[] unvan2;
+        //string[] telNo2;
+        //string[] adres2;
+        //double[] bakiye2;
+        //string[] mail2;
+        ArrayList musteriler = new ArrayList();
+
+        private void btnKaydet_Click(object sender, EventArgs e)
+        {
+            if (DegerleriAl())
+            {
+                string musteri = musteriNo.ToString() + "\t" + unvan + "\t" + telNo + "\t" + adres + "\t" + bakiye.ToString() + "\t" + mail;
+                listBox1.Items.Add(musteri);
+                musteriler.Add(musteri);
+                AlanlariTemizle();
+            }
+            
+        }
+
+        public bool DegerleriAl()
+        {
+            try
+            {
+                if (!TelefonKontrol(tbTelefon.Text))
+                {
+                    tbTelefon.Clear();
+                    MessageBox.Show("Telefon numarası yanlış formatta!!\nİlgili alanı tekrardan doldurun!!");
+                    return false;
+                }
+                else if(!IsValid(tbMail.Text))
+                {
+                    tbMail.Clear();
+                    MessageBox.Show("Mail adresi formata uygun değil!!\nİlgili alanı tekrardan doldurun!!");
+                    return false;
+                }
+                else
+                {
+                    musteriNo = Convert.ToInt32(tbMusteriNo.Text);
+                    unvan = tbUnvan.Text;
+                    telNo = tbTelefon.Text;
+                    adres = tbAdres.Text;
+                    mail = tbMail.Text;
+                    bakiye = Convert.ToDouble(tbBakiye.Text);
+                    return true;
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Bilgiler Hatalı ya da Boş Olamaz!!!");
+                return false;
+            }
+            
+        }
+
+        public void AlanlariTemizle()
+        {
+            tbMusteriNo.Clear();
+            tbUnvan.Clear();
+            tbTelefon.Clear();
+            tbAdres.Clear();
+            tbBakiye.Clear();
+            tbMail.Clear();
+        }
+
+        public bool IsValid(string emailaddress)
+        {
+            try
+            {
+                MailAddress m = new MailAddress(emailaddress);
+
+                return true;
+            }
+            catch (FormatException)
+            {
+                return false;
+            }
+        }
+
+
+        public static bool TelefonKontrol(string numara)
+        {
+            string regex = @"^(0(\d{3})-(\d{3})-(\d{2})-(\d{2}))$";
+
+            if (numara != null) return Regex.IsMatch(numara, regex);
+            else return false;
+        }
+
+        private void btnSil_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Remove(listBox1.SelectedItem);
+        }
+        private void btnAranan_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var index = new List<int>();
+                int sayac = 0;
+                string[] sub = new string[6];
+                for (int i = 0; i < listBox1.Items.Count; i++)
+                {
+                    sub = (listBox1.Items[i].ToString()).Split('\t');
+                    if (sub.Contains(tbAranan.Text, StringComparer.CurrentCultureIgnoreCase))
+                    {
+                        index.Add(i);
+                        sayac++;
+                    }
+                }
+                if (sayac != 1)
+                {
+                    MessageBox.Show("Lütfen başka bir değer arayınız");
+                }
+                else
+                {
+                    int y = index[0];
+                    listBox1.SetSelected(y, true);
+                }
+            }
+            catch 
+            {
+                MessageBox.Show("Hatalı giriş yapıldı!!");
+            }
+            
+        }
+
+        private void listBox1_Click(object sender, EventArgs e)
+        {
+             DegerleriYaz();
+           
+        }
+
+        private void DegerleriYaz()
+        {
+            try
+            {
+                string[] sub = new string[6];
+                sub = (listBox1.SelectedItem.ToString()).Split('\t');
+                tbMusteriNo.Text = sub[0];
+                tbUnvan.Text = sub[1];
+                tbTelefon.Text = sub[2];
+                tbAdres.Text = sub[3];
+                tbBakiye.Text = sub[4];
+                tbMail.Text = sub[5];
+            }
+            catch
+            {
+
+            }
+            
+        }
+
+        private void btnDüzenle_Click(object sender, EventArgs e)
+        {
+            DegerleriAl();
+            string musteri = musteriNo.ToString() + "\t" + unvan + "\t" + telNo + "\t" + adres + "\t" + bakiye.ToString() + "\t" + mail;
+            int index = listBox1.SelectedIndex;
+            listBox1.Items.Remove(listBox1.SelectedItem);
+            listBox1.Items.Insert(index, musteri);
+
+        }
+
+        private void btnOncekiKayit_Click(object sender, EventArgs e)
+        {
+            listBox1.SelectedIndex -= 1;
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            DegerleriYaz();
+            int index = listBox1.SelectedIndex;
+            if (index == 0)
+            {
+                btnSonrakiKayit.Enabled = true;
+                btnOncekiKayit.Enabled = false;
+            }
+            else if(index > 0 && index != listBox1.Items.Count - 1)
+            {
+                btnOncekiKayit.Enabled = true;
+                btnSonrakiKayit.Enabled = true;
+            }
+            else if(index == listBox1.Items.Count-1)
+            {
+                btnSonrakiKayit.Enabled = false;
+            }
+        }
+
+        private void btnSonrakiKayit_Click(object sender, EventArgs e)
+        {
+            listBox1.SelectedIndex += 1;
+        }
+    }
+}
